e2b0409b39369c328659da6f0707a98b31638559
sumanth-hegde <sumanthsh1998@gmail.com>
2025-05-12 22:06:52 +0530

    recursion examples

--- reverse_number.py
+++ reverse_number.py
@@ -34,6 +34,21 @@ def test6(i, n):
     print("Backtracking n to 1 :", i)
     test6(i-1, n)
 
+class Solution:
+    def n_numbers_sum(self, N):
+        if N == 1:
+            return 1
+        return N + self.n_numbers_sum(N - 1)
+
+class Fibonacci:
+    def print_fibo(self, n):
+        fibo = []
+        if n <= 1:
+            return n
+        val = self.print_fibo(n-1) + self.print_fibo(n-2)
+        fibo.append(val)
+        return fibo
+
 if __name__ == "__main__":
     print(test(0, [1,2,3,4,5], 0))
     print(test2(5, "Sumanth"))
@@ -41,3 +56,7 @@ if __name__ == "__main__":
     print(test4(0, 5))
     print(test5(3, 3))
     print(test6(3, 3))
+    sol = Solution()
+    print(sol.n_numbers_sum(4))
+    fib = Fibonacci()
+    print(fib.print_fibo(10))
