8e2db575af6289774c7c20dc0e0350bb90c5fa4a
sumanth-hegde <sumanthsh1998@gmail.com>
2025-05-19 17:26:51 +0530

    recursive bubble sort optimised

--- recursive_bubble_sort.py
+++ recursive_bubble_sort.py
@@ -1,16 +1,15 @@
 class RecursiveBubbleSort:
     def solution(self, arr, n):
         if n == 1:
-            return
+            return arr
         swap = False
         for i in range(n-1):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
                 swap = True
         if not swap:
-            return
-        self.solution(arr, n-1)
-        return arr
+            return arr
+        return self.solution(arr, n-1)
 
 sort = RecursiveBubbleSort()
 arr = [3,2,1,4,5]
